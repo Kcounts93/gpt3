89fe6af6c41127006323ded4a8c9c2e173fc1267
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-06-02 19:43:11 -0700

    CTA component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='gpt3__cr=ta'>
+      <div className='gpt3__cta-content'>
+        <p>request Early Access</p>
+        <h3>Register today & start exploring the endless possibilities with AI</h3>
+      </div>
+      <div className='gpt3__cta-button'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
