fa28f590213e5f4217f566e37708ee66141b0ba3
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-06-02 20:57:53 -0700

    cta links href

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step into the future before others?</h1>
+      </div>
+      <div className='gpt__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt='logo'/>
+          <p>3180 18th Street, San Francisco, CA 94110 All Rights Reserved </p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p></p>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
