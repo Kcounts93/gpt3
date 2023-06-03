6e99af9e26a0b4a1670b9b4d1cb6fc57dc67def5
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-06-02 20:21:38 -0700

    blog article styles & article div

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -10,10 +10,12 @@ const Article = ({ imgURL, date, title }) => {
         <img src={imgURL} alt='blog'/>
       </div>
       <div className='gpt3__blog-container_article-content'>
+        <div>
         <p>{date}</p>
         <h3>{title}</h3>
       </div>
-      <p>Read Full Article</p>
+        <p>Read Full Article</p>
+      </div>
     </div>
   )
 }
